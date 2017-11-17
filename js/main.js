873c14733c16e5cf873f335f97ae47c6eb8394ec
aniaouek <aniaouek@gmail.com>
2017-11-17 15:10:04 +0100

    init

--- js/main.js
+++ js/main.js
@@ -113,19 +113,79 @@ document.body.removeChild(btn);
 //manipulacja DOM
 var header = document.getElementById('header');
 console.log(header.innerHTML);
-header.innerHTML = '<p>Nowa tresc w naglowku</p>';
+//header.innerHTML = '<p>Nowa tresc w naglowku</p>';
+header.innerHTML = '<p class="nowa_klasa">Nowa tresc w naglowku</p>';
 
 
+//mozemy dodawac znaczniki za pomoca outerHTML - lacznie ze znacznikiem do ktorego sie odnosimy
+console.log(header.outerHTML);
+header.outerHTML = '<header id="header" class="klasa"> <p>123123123</p></header>';
 
+//pobieranie/zmienianie atrybutu
+console.log(linki[2].href);
+console.log(linki[2].className);
 
+//class-name -ustawia/pobiera klasy elementu (mozna podac wiecej niz jeden)
+//jesli modyfikujemy za pomoca outerHTML, to nie jestesmy w stanie tego nadpisac
+header.className = 'klasa_2 klasa_3';
+console.log(header.className);
 
+//header.className = 'klasa_2';
+//console.log(header.className);	
 
+linki[2].href = 'http://google.com';
+console.log(linki[2].href);
 
+linki[2].title = 'tytul';
+linki[2].className = 'klasa_2 klasa_3';
 
+var linkCSS = document.getElementById('link');
 
+linkCSS.style.color = 'red';
+linkCSS.style.fontSize = '25px';
 
+//eventy
 
+//czeka na zaladowanie drzewa DOM
+document.addEventListener("DOMContentLoaded", function () {
+	//	tutaj trafia skryp operujacy na elementach ze strony
+	zmienKolor();
+	klawiszWcisniety();
+});
 
+function zmienKolor() {
+	//	linkCSS.addEventListener('mouseover',function() {
+	//		this.style.color = 'violet';
+	//	});
+	//
+	//	linkCSS.addEventListener('mouseout',function() {
+	//		this.style.color = 'red';
+	//	});
+
+	for (var i = 0; i < linki.length; i++) {
+		linki[i].addEventListener('mouseover', function () {
+			this.style.color = 'violet';
+		});
+
+		linki[i].addEventListener('mouseout', function () {
+			this.style.color = 'green';
+		});
+	}
+}
+
+function klawiszWcisniety() {
+	var inputy = document.querySelectorAll('#formularz input');
+	console.log(inputy);
+
+	for (var i = 0; i < inputy.length; i++) {
+		inputy[i].addEventListener('keydown', function () {
+			this.style.border = '3px solid red';
+		});
+		inputy[i].addEventListener('keyup', function () {
+			this.style.border = '10px solid blue';
+		});
+	}
+}
 
 
 
